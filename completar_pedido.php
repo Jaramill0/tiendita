b38245d27a68dfe9102da6256587f83cb9102780
Jaramill0 <hugo98jaramillo@gmail.com>
2023-01-31 13:25:13 -0600

    Completando el pedido del articulo y el carrito de compras

--- completar_pedido.php
+++ completar_pedido.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-
+//importando el archivo de funciones
+require 'funciones.php';
+//importando el autoload
+require 'vendor/autoload.php';
+//metodo post para importar el carrito de productos
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require 'funciones.php';
-
-    require 'vendor/autoload.php';
-
     if (isset($_SESSION['carrito']) && !empty($_SESSION['carrito'])) {
 
         $cliente = new Kawschool\Cliente;
@@ -16,9 +16,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'telefono' => $_POST['telefono'],
             'comentario' => $_POST['comentario']
         );
-
+        //metodo clase de registrar
         $cliente_id = $cliente->registrar($_params);
-
+        //colocando la clase pedido para invocar el calculo del total 
         $pedido = new Kawschool\Pedido;
 
         $_params = array(
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'fecha' => date('Y-m-d')
         );
         $pedido_id = $pedido->registrar($_params);
-
+        //array para registrar los datos del pedido 
         foreach ($_SESSION['carrito'] as $indice => $value) {
             $_params = array(
                 'pedidos_id' => $pedido_id,
