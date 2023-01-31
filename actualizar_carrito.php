3ee6acc3b2794344a1b6994d98bd5f65bda774f2
Jaramill0 <hugo98jaramillo@gmail.com>
2023-01-31 13:16:56 -0600

    Archivo para actualizar la cantidad del producto en el carrito de compras

--- actualizar_carrito.php
+++ actualizar_carrito.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-
+require 'funciones.php';
+//metodo para actualizar  la cantidad del producto
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require 'funciones.php';
     $id = $_POST['id'];
     $cantidad = $_POST['cantidad'];
-
     if (is_numeric($cantidad)) {
         if (array_key_exists($id, $_SESSION['carrito'])) 
             actualizarPelicula($id,$cantidad);
